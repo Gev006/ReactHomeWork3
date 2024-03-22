c692a0a2fdf912096304f228a09bcefd41e61e40
GevS <gevs.sargsyan96@gmail.com>
2024-03-22 15:52:33 +0400

    add minesweeper

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import TicTacToe from './TicTacToe/tictactoe'
+import Minesweeper from './Minesweeper/minesweeper/minesweeper'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <TicTacToe/>
+    {/* <TicTacToe/> */}
+    <Minesweeper/>
   </React.StrictMode>,
 )
