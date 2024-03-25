c1cbe15f648921e98bac4e64f08e15c027dde60e
GevS <gevs.sargsyan96@gmail.com>
2024-03-25 17:44:42 +0400

    add tictactoe_2

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import TicTacToe from './TicTacToe/tictactoe'
-import Minesweeper from './Minesweeper/minesweeper/minesweeper'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {/* <TicTacToe/> */}
-    <Minesweeper/>
-  </React.StrictMode>,
+<App/>
 )
